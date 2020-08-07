64605428d9781a5b2836e44fac3839dfefb82447
daiki-fujii <53123867+daiki-fujii@users.noreply.github.com>
2020-08-07 23:59:24 +0900

    ZOOM CLONE DONE

---
+++ public/script.js
@@ -0,0 +1,119 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+let MyVideoStream
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const constraints = {
+  video: true,
+  audio: true
+}
+const messages = document.getElementsByClassName('messages')[0]
+const peer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '443'
+})
+
+getMedia(constraints)
+
+peer.on('open', id => {
+  socket.emit('join-room', roomId, id)
+})
+
+socket.on('createMessage', async message => {
+  let msg = document.createElement('li')
+  msg.innerText = await message
+  messages.appendChild(msg)
+  scrollToButton()
+})
+
+async function getMedia(constraints) {
+  let stream = null
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(constraints)
+    MyVideoStream = stream
+    addVideoStream(myVideo, stream)
+    peer.on('call', call => {
+      call.answer(stream)
+      const video = document.createElement('video')
+      call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+      })
+    })
+    socket.on('user-connected', (userId) => {
+      connectToNewUser(userId, stream)
+    })
+  } catch (err) {
+    window.alert(err)
+  }
+}
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.appendChild(video)
+}
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+}
+
+const sendMessage = () => {
+  let msg = document.getElementById('chat_message')
+  socket.emit('message', msg.value)
+  msg.value = ''
+  return false
+}
+
+const scrollToButton = () => {
+  const d = document.getElementsByClassName('main__chat_window')[0]
+  d.scrollTop = d.scrollHeight
+}
+
+const muteUnmute = () => {
+  const enabled = MyVideoStream.getAudioTracks()[0].enabled
+  if (enabled) {
+    MyVideoStream.getAudioTracks()[0].enabled = false
+    setUnmuteButton()
+  } else {
+    setMuteButton()
+    MyVideoStream.getAudioTracks()[0].enabled = true
+  }
+}
+
+const setMuteButton = () => {
+  const html = '<i class="fas fa-microphone"></i><span>Mute</span>'
+  document.getElementsByClassName('main__mute_button')[0].innerHTML = html
+}
+
+const setUnmuteButton = () => {
+  const html = '<i class="unmute fas fa-microphone-slash"></i><span>Unmute</span>'
+  document.getElementsByClassName('main__mute_button')[0].innerHTML = html
+}
+
+const playStop = () => {
+  const enabled = MyVideoStream.getVideoTracks()[0].enabled
+  if (enabled) {
+    MyVideoStream.getVideoTracks()[0].enabled = false
+    setPlayVideo()
+  } else {
+    setStopVideo()
+    MyVideoStream.getVideoTracks()[0].enabled = true
+  }
+}
+
+const setStopVideo = () => {
+  const html = '<i class="fas fa-video"></i><span>Stop Video</span>'
+  document.getElementsByClassName('main__play_button')[0].innerHTML = html
+}
+
+const setPlayVideo = () => {
+  const html = '<i class="stop fas fa-video-slash"></i><span>Play Video</span>'
+  document.getElementsByClassName('main__play_button')[0].innerHTML = html
+}
