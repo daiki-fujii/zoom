f6ec7aabcaa34f529f7296c35805c4c0e1f3cba9
daiki-fujii <53123867+daiki-fujii@users.noreply.github.com>
2020-08-10 19:38:26 +0900

    autoplay muted playsinline to socket.io

--- public/script.js
+++ public/script.js
@@ -42,6 +42,9 @@ async function getMedia(constraints) {
     peer.on('call', call => {
       call.answer(stream)
       const video = document.createElement('video')
+      myVideo.setAttribute('muted', '')
+      myVideo.setAttribute('autoplay', '')
+      myVideo.setAttribute('playsinline', '')
       call.on('stream', userVideoStream => {
         addVideoStream(video, userVideoStream)
       })
